e9448abb0f88533ad89f58cb9f0bf7d901685eee
unknown <darifrahmanda@gmail.com>
2021-01-26 03:30:41 +0700

    update format email send qrcode

--- app/Views/send_email.php
+++ app/Views/send_email.php
@@ -16,6 +16,15 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="/assets/ClassyQr/js/jquery.classyqr.js"></script>
+    <script>
+        let noreg = "<?= $detail_customer['customer_unique']; ?>";
+        $('#qrcode').ClassyQR({
+            create: true,
+            type: 'url',
+            text: noreg
+        });
+    </script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/promise-polyfill"></script> -->
 
     <title>QUICKTEST.ID - <?= $title; ?></title>
@@ -30,16 +39,18 @@
             <div class="mailbox-read-message">
                 <h3>Quick Test Covid</h3>
                 <p>Hello <?= $detail_customer['nama']; ?>,</p>
-                <p></p>
+
                 <?php
                 $responseMessage = "";
                 $transType = $notif->transaction_status;
                 if ($transType == "settlement" || $transType == "capture") {
-                    $responseMessage = "Terima kasih telah melakukan pembayaran pada kami, berikut kami berikan gambar QR Code untuk anda 
+                    $responseMessage = "<P>Terima kasih telah melakukan pembayaran pada kami, berikut kami berikan gambar QR Code untuk anda 
                     hadir pada tempat kami saat melakukan konfirmasi kedatangan pada tanggal " . $detail_customer['tgl_kunjungan'] .
-                        " pukul " . $detail_customer['jam_kedatangan'];
+                        " pukul " . $detail_customer['jam_kedatangan'] . ".</p>";
+                    echo $responseMessage;
                 }
                 ?>
+                <div id="qrcode"></div>
                 <p>Thanks,<br>QuickTest Info</p>
             </div>
             <!-- /.mailbox-read-message -->
