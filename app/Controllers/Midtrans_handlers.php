8b95aeb64a69a548e200ef45cf234a794495fff3
unknown <darifrahmanda@gmail.com>
2021-02-06 13:58:20 +0700

    try api notif

--- app/Controllers/Midtrans_handlers.php
+++ app/Controllers/Midtrans_handlers.php
@@ -56,7 +56,35 @@ class Midtrans_handlers extends ResourceController
             $responseStatus = "success";
             $customer_check = $this->CustomerModel->where(['customer_unique' => $order_id])->get()->getRowArray();
             // return $customer_check;
-
+            if ($transaction == 'capture') {
+                // For credit card transaction, we need to check whether transaction is challenge by FDS or not
+                if ($type == 'credit_card') {
+                    if ($fraud == 'challenge') {
+                        // TODO set payment status in merchant's database to 'Challenge by FDS'
+                        // TODO merchant should decide whether this transaction is authorized or not in MAP
+                        echo "Transaction order_id: " . $order_id . " is challenged by FDS";
+                    } else {
+                        // TODO set payment status in merchant's database to 'Success'
+                        echo "Transaction order_id: " . $order_id . " successfully captured using " . $type;
+                    }
+                }
+            } else if ($transaction == 'settlement') {
+                // TODO set payment status in merchant's database to 'Settlement'
+                echo "Transaction order_id: " . $order_id . " successfully transfered using " . $type;
+            } else if ($transaction == 'pending') {
+                // TODO set payment status in merchant's database to 'Pending'
+                echo "Waiting customer to finish transaction order_id: " . $order_id . " using " . $type;
+            } else if ($transaction == 'deny') {
+                // TODO set payment status in merchant's database to 'Denied'
+                echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is denied.";
+            } else if ($transaction == 'expire') {
+                // TODO set payment status in merchant's database to 'expire'
+                echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is expired.";
+            } else if ($transaction == 'cancel') {
+                // TODO set payment status in merchant's database to 'Denied'
+                echo "Payment using " . $type . " for transaction order_id: " . $order_id . " is canceled.";
+            }
+            exit();
             if ($customer_check) {
                 $id_customer = $customer_check['id'];
 
