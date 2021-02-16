4e188c09dd926d2e8dc4951e7a369826d84c5888
unknown <darifrahmanda@gmail.com>
2021-02-16 17:09:22 +0700

    table borderless on table hasil antibody

--- app/Views/backoffice/laboratorium/print_hasil_pesertav2.php
+++ app/Views/backoffice/laboratorium/print_hasil_pesertav2.php
@@ -83,127 +83,137 @@
             </tr>
         </table>
     </footer>
-    <main class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <table class="table table-sm">
-                    <tr>
-                        <td>No. Reg/ Reg. No</td>
-                        <td>:</td>
-                        <td><?= $customer['customer_unique']; ?></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <table class="table table-sm">
-                    <tr>
-                        <td>Nama</td>
-                        <td>: <?= $customer['nama']; ?></td>
-                        <td>Kewarganegaraan</td>
-                        <td>: </td>
-                    </tr>
-                    <tr>
-                        <td>NIK</td>
-                        <td>: <?= $customer['nik']; ?></td>
-                        <td>Alamat</td>
-                        <td rowspan="3">: <?= $customer['alamat']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal Lahir/DOB</td>
-                        <td>: <?= date("F, d Y", strtotime($customer['tanggal_lahir'])); ?></td>
-                    </tr>
-                    <tr>
-                        <td>Jenis Kelamin/Sex</td>
-                        <td>: <?= ucwords($customer['jenis_kelamin']); ?></td>
-                    </tr>
-                    <tr>
-                        <td>Waktu Sampling</td>
-                        <td>: <?= date("F,d Y", strtotime($detail_hasil['waktu_ambil_sampling'])); ?></td>
-                        <td>Faskes</td>
-                        <td>: <?= $nama_faskes; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Waktu Periksa</td>
-                        <td>: <?= date("F, d Y", strtotime($detail_hasil['waktu_periksa_sampling'])); ?></td>
-                        <td>Kota/<i>City</i></td>
-                        <td>: <?= $city; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Waktu Selesai</td>
-                        <td>: <?= date("F, d Y", strtotime($detail_hasil['waktu_selesai_periksa'])); ?></td>
-                        <td>Instansi</td>
-                        <td>: <?= ucwords($customer['nama_instansi']); ?></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <table class="table table-borderless table-sm" id="data_customer">
-                    <thead>
-                        <tr class="bg-primary">
-                            <th>PEMERIKSAAN/TEST</th>
-                            <th>HASIL/RESULT</th>
-                            <th>BATAS NORMAL/NORMAL RANGE</th>
-                            <th>METODE/METHOD</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+    <main>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <table class="table table-borderless table-sm">
                         <tr>
-                            <td><strong>Antigen SARS-Cov-2</strong></td>
-                            <td></td>
-                            <td><?= $detail_hasil['status_cov']; ?></td>
-                            <td></td>
+                            <td width="25%">Pemeriksa/<i>Investigator</i></td>
+                            <td>: <?= $customer['customer_unique']; ?></td>
                         </tr>
                         <tr>
-                            <td>Interpretasi</td>
-                            <td colspan="3">Result RT-PCR SARS-CoV-2: <?= $detail_hasil['status_cov']; ?></td>
+                            <td>Dokter/<i>Doctor</i></td>
+                            <td>: <?= $doctor_name; ?></td>
                         </tr>
                         <tr>
-                            <td>Rekomendasi</td>
-                            <td colspan="3"></td>
+                            <td>No. Reg/<i>Reg. No</i></td>
+                            <td>: <?= $customer['customer_unique']; ?></td>
                         </tr>
-                    </tbody>
-                </table>
+                    </table>
+                </div>
             </div>
-        </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-borderless table-sm">
+                        <tr>
+                            <td>Nama</td>
+                            <td>: <?= $customer['nama']; ?></td>
+                            <td>Kewarganegaraan</td>
+                            <td>: </td>
+                        </tr>
+                        <tr>
+                            <td>NIK</td>
+                            <td>: <?= $customer['nik']; ?></td>
+                            <td>Alamat</td>
+                            <td rowspan="3">: <?= $customer['alamat']; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal Lahir/DOB</td>
+                            <td>: <?= date("F, d Y", strtotime($customer['tanggal_lahir'])); ?></td>
+                        </tr>
+                        <tr>
+                            <td>Jenis Kelamin/Sex</td>
+                            <td>: <?= ucwords($customer['jenis_kelamin']); ?></td>
+                        </tr>
+                        <tr>
+                            <td>Waktu Sampling</td>
+                            <td>: <?= date("F,d Y", strtotime($detail_hasil['waktu_ambil_sampling'])); ?></td>
+                            <td>Faskes</td>
+                            <td>: <?= $nama_faskes; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Waktu Periksa</td>
+                            <td>: <?= date("F, d Y", strtotime($detail_hasil['waktu_periksa_sampling'])); ?></td>
+                            <td>Kota/<i>City</i></td>
+                            <td>: <?= $city; ?></td>
+                        </tr>
+                        <tr>
+                            <td>Waktu Selesai</td>
+                            <td>: <?= date("F, d Y", strtotime($detail_hasil['waktu_selesai_periksa'])); ?></td>
+                            <td>Instansi</td>
+                            <td>: <?= ucwords($customer['nama_instansi']); ?></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-borderless table-sm" style="border: 1pt solid #333;" id="data_customer">
+                        <thead>
+                            <tr class="bg-primary">
+                                <th>PEMERIKSAAN/TEST</th>
+                                <th>HASIL/RESULT</th>
+                                <th>BATAS NORMAL/NORMAL RANGE</th>
+                                <th>METODE/METHOD</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><strong>Antigen SARS-Cov-2</strong></td>
+                                <td></td>
+                                <td><?= $detail_hasil['status_cov']; ?></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>Interpretasi</td>
+                                <td colspan="3">Result RT-PCR SARS-CoV-2: <?= $detail_hasil['status_cov']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Rekomendasi</td>
+                                <td colspan="3"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
-        <h4>DISCLAIMER:</h4>
-        Catatan: <br>
-        <ol>
-            <li>Hasil pemeriksaan di atas dikerjakan dengan metode Swab Antigen dan hanya menggambarkan kondisi saat pengambilan spesimen.</li>
-            <li>Hasil PCR negatif tidak selalu berarti pasien tidak terinfeksi oleh pathogen, namun hanya menunjukkan bahwa material denetik pathogen tidak ditemukan di dalam
-                sample.</li>
-            <li>Jika hasil PCR positif atau timbul gejala klinis setelah pemeriksaan, silahkan hubungi dokter atau fasilitas kesehatan terdekat.</li>
-            <li>Hasil PCR tidak dapat diandingkan antar satu laboratorium dengan laboratorium lain, karena perbedaan alat dan target gen yang digunakan.</li>
-            <li>Surat ini dikirim dan ditandatangani secara elektronik.</li>
-        </ol>
-        note:
-        <ol>
-            <li>The above examination results were carried out using the Antigen Swab method and only described the conditions at the time of specimen collection.</li>
-            <li>A negative Antigen Swab result does not necessarily mean that the patient is not infected with the pathogen, but only indicates that pathogenic genetic material is not present
-                in the sample.</li>
-            <li>If the PCR result is positive or clinical symptoms appear after the examination, please contact the doctor or the nearest health facility.</li>
-            <li>Antigen Swab results cannot be compared between one laboratory and another, due to differences in the tools and target genes used.</li>
-            <li>This letter is sent and signed electronically</li>
-        </ol>
-        <div class="row">
-            <div class="col-md-12">
-                <table class="table table-borderless">
-                    <tr>
-                        <td class="text-center">QRCODE RESULT<br>
-                            <img src="<?= $image_qr_result ?>" alt="">
-                        </td>
-                        <td class="text-center"><?= $city; ?>, <?= $sign_waktu; ?><br>
+            <h4>DISCLAIMER:</h4>
+            Catatan: <br>
+            <ol>
+                <li>Hasil pemeriksaan di atas dikerjakan dengan metode Swab Antigen dan hanya menggambarkan kondisi saat pengambilan spesimen.</li>
+                <li>Hasil PCR negatif tidak selalu berarti pasien tidak terinfeksi oleh pathogen, namun hanya menunjukkan bahwa material denetik pathogen tidak ditemukan di dalam
+                    sample.</li>
+                <li>Jika hasil PCR positif atau timbul gejala klinis setelah pemeriksaan, silahkan hubungi dokter atau fasilitas kesehatan terdekat.</li>
+                <li>Hasil PCR tidak dapat diandingkan antar satu laboratorium dengan laboratorium lain, karena perbedaan alat dan target gen yang digunakan.</li>
+                <li>Surat ini dikirim dan ditandatangani secara elektronik.</li>
+            </ol>
+            note:
+            <ol>
+                <li>The above examination results were carried out using the Antigen Swab method and only described the conditions at the time of specimen collection.</li>
+                <li>A negative Antigen Swab result does not necessarily mean that the patient is not infected with the pathogen, but only indicates that pathogenic genetic material is not present
+                    in the sample.</li>
+                <li>If the PCR result is positive or clinical symptoms appear after the examination, please contact the doctor or the nearest health facility.</li>
+                <li>Antigen Swab results cannot be compared between one laboratory and another, due to differences in the tools and target genes used.</li>
+                <li>This letter is sent and signed electronically</li>
+            </ol>
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-borderless">
+                        <tr>
+                            <td class="text-center">QRCODE RESULT<br>
+                                <img src="<?= $image_qr_result ?>" alt="">
+                            </td>
+                            <td class="text-center"><?= $city; ?>, <?= $sign_waktu; ?><br>
 
-                            Dokter Pemeriksa/<i>Physician Examiner</i>
-                            <br>
-                            <?= $doctor_name; ?>
-                        </td>
-                    </tr>
-                </table>
+                                Dokter Pemeriksa/<i>Physician Examiner</i>
+                                <br>
+                                <?= $doctor_name; ?>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </main>
