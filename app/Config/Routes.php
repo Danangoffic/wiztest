9b403eaac7ff7e45375e8900b9e5f5063f7a4e1d
unknown <darifrahmanda@gmail.com>
2021-02-16 20:54:05 +0700

    routes backoffice only

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,7 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 
-$routes->get('/', 'Customer::index');
+$routes->get('/', 'backoffice\Home::index');
 $routes->add('/backoffice', 'backoffice\Home::index');
 $routes->get('/backoffice/login', 'backoffice\User::login');
 $routes->post('/backoffice/doLogin', 'backoffice\User::doLogin');
